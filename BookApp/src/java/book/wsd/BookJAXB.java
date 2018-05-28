26f370045e5561a890b6a538702b11b0e1be2e1d
brada <gamyuen@hotmail.com>
2018-05-28 22:31:39 +1000

    follow the demo project to make namespace

--- BookApp/src/java/book/wsd/BookJAXB.java
+++ BookApp/src/java/book/wsd/BookJAXB.java
@@ -17,9 +17,9 @@ public class BookJAXB implements Serializable {
         Publication publication2 = new Publication("a health book", "988-1-70-146450-3", "5th edition", 2015, "wiley");
         Publication publication3 = new Publication("a law book", "988-1-70-146450-3", "7th edition", 2015, "wiley");
 
-        bookshop.addBook(new Book(1, "XML Developer's Guide", "Matthew Gambardella", "Computer", 3, 40.0, "Good", publication1));
+        bookshop.addBook(new Book(1, "XML Developer's Guide", "Matthew Gambardella", "Computer", 1, 40.0, "Good", publication1));
         bookshop.addBook(new Book(2, "Exercise Physiology", "Mcardle", "Health", 1, 35.0, "fair", publication2));
-        bookshop.addBook(new Book(3, "The New Law of Torts", "Danuta Mendelson", "Legal", 2, 55.0, "fair", publication3));
+        bookshop.addBook(new Book(3, "The New Law of Torts", "Danuta Mendelson", "Legal", 1, 55.0, "fair", publication3));
 
  //Boilerplate code to convert objects to XML...
         JAXBContext jc = JAXBContext.newInstance(Books.class);
--- BookApp/src/java/book/wsd/Books.java
+++ BookApp/src/java/book/wsd/Books.java
@@ -14,7 +14,7 @@ import javax.xml.bind.annotation.*;
 public class Books implements Serializable {
 
     @XmlElement(name = "book")
-    private List<Book> books = new ArrayList<Book>();
+    private ArrayList<Book> books = new ArrayList<Book>();
 
     public Books() {
     }
